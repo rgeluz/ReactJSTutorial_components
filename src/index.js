9a4aec6cafe67cda7672e04f0f36b07059471b24
Roman <rgeluz22@yahoo.com>
2020-10-03 15:02:33 -0700

    added CommentDetail.js componpent

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
 
 const App = () => {
-  return <div>Hi There!</div>
+  return (
+    <div className="ui container comments">
+      {/* comment # 1 */}
+      <CommentDetail />
+
+      {/* comment # 2 */}
+      <CommentDetail />
+
+      {/* comment # 3 */}
+      <CommentDetail />
+
+      {/* comment # 4 */}
+      <CommentDetail />
+      
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
